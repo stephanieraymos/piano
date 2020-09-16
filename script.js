c73b698d5a6a1e6b7f80820bb05187b9c100996c
Stephanie Raymos <stephanieraymos@gmail.com>
2020-09-15 20:01:39 -0700

    Setting up keyboard keys

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
 const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 
 const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
 
 keys.forEach(key => {
   key.addEventListener('click', () => playNote(key))
+})
+
+document.addEventListener('keydown', e => {
+  const key = e.key
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+  const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+  if (whiteKeyIndex > -1 /*-1 is returned when nothing can be found*/) playNote()
 })
 
 function playNote(key) {
